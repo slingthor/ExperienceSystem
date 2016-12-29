50db5dd33ba2b0e83095e90f29b6635153eaa8bd
DESKTOP-5M6QIH2\ingth <ingthorh@gmail.com>
2016-12-29 22:10:00 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<long,long> progressionFunc = new Func<long, long>(l => l/100);
+            Progression progression = new Progression(progressionFunc);
+
+            var xp = progression.GetExperienceForLevel(3);
+
+            Console.WriteLine(xp);
+            string x =Console.ReadLine();
+        }
+    }
+}
